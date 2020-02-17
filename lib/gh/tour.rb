e382272b15c9b5f66ec89c35e31897a92e9b567c
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-17 07:56:34 +0000

    first commit

---
+++ lib/gh/tour.rb
@@ -0,0 +1,8 @@
+require "gh/tour/version"
+
+module Gh
+  module Tour
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/gh/tour/version.rb
@@ -0,0 +1,5 @@
+module Gh
+  module Tour
+    VERSION = "0.1.0"
+  end
+end
